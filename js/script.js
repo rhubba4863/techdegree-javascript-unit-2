a22880b0443c71fb540f32483c1dce208c427a50
rhubba4863 <rph_1994@yahoo.com>
2023-03-27 10:35:50 -0400

    Adding Pagination Buttons
    only added the buttons. students are not yet changed on page.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 document.addEventListener('DOMContentLoaded', () => {
    const ul = document.querySelector(".student-list");
+   const ulButton = document.querySelector(".link-list");
+   let currentStart = 0;
+   let currentEnd = 0;
+   let counter = 0; //RPH: might not be needed
+
+   function buttonTotal(){
+      const totalButtons = Math.ceil(studentList.length/9);
+      console.log("pages expected "+(totalButtons));
+      return totalButtons;
+   }
 
    /*
    For assistance:
@@ -18,8 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
    This function will create and insert/append the elements needed to display a "page" of nine students
    */
 
-
-   let spot;
+   //let spot;
    const studentList = data;
    const totalStudents = studentList.length;
 
@@ -27,8 +36,10 @@ document.addEventListener('DOMContentLoaded', () => {
    console.log("RPH1 checking student name:", studentList[1].name.last);
 
    function createStudentLI(spot){
-      const studentToCopy = studentList[spot];
+      const oneStudent = studentList[spot];
+      const name = oneStudent.name.last + " " + oneStudent.name.first;
 
+      //2 steps to create the elements
       function createElement(elementName, property, value) {
          const element = document.createElement(elementName);
          element[property] = value;
@@ -39,23 +50,31 @@ document.addEventListener('DOMContentLoaded', () => {
          const element = createElement(elementName, property, value);
          li.appendChild(element);
          return element;
-       }
+      }
 
-      //Final setup of li element
-      const li = createElement('li', 'class', "student-item cf");
-      appendToLI('div', 'class', "student-details");
-      appendToLI('div', 'class', "student-details");
+      //Final setup of each element piece within li element
+      const li = createElement('li', 'className', "student-item cf");
+      const myImage = createElement('img', 'className', 'avatar');
+      myImage.setAttribute("src", oneStudent.picture.medium);
+      const myName = createElement('h3');
+      myName.textContent = name;
+      let contact = createElement('span', 'className', 'email');
+      contact.textContent = oneStudent.email;
+     
+      //Pieces all the elements together
+      appendToLI('div', 'className', "student-details")
+        .append(myImage, myName, contact);
 
+      let joinDate = createElement('span', 'className', 'date');
+      joinDate.textContent = "Joined " + oneStudent.registered.date;  
+      appendToLI('div', 'className', "joined-details")
+         .append(joinDate);
+      
+     // console.log(li);
       return li;
    }
 
-//// li.className = 'AAA';
-
-   //   const ul = document.getElementsByClassName("student-list");
-   //const lin = createStudentLI(2)
-   //ul.appendChild(document.createElement("div"));
-   ul.appendChild(createStudentLI(2));
-
+/*
    // EXAMPLE - Student list item:
    // <li class="student-item cf">
    //   <div class="student-details">
@@ -67,28 +86,81 @@ document.addEventListener('DOMContentLoaded', () => {
    //     <span class="date">Joined 12-15-2005</span>
    //   </div>
    // </li>
+*/
 
-   //appendToLI('span', 'textContent', "phubbs");
-   //appendToLI('div', 'class', "student-details");
-   // appendToLI('span', 'textContent', text);
-   // appendToLI('label', 'textContent', 'Confirmed')
-   //    .appendChild(createElement('input', 'type', 'checkbox'));
-   // appendToLI('button', 'textContent', 'edit');
-   // appendToLI('button', 'textContent', 'remove');
-   function sendStudents(){
+   function showPage(){
+      let start = currentStart; 
+      let end = 9; //edit later
+      let remainingStudents = (totalStudents - currentStart)
 
+      //check if 9 still can are shown
+      if(remainingStudents < 9){
+        end = totalStudents;
+      }else if (remainingStudents == 0){
+         start = 0;
+         end = 9;
+      }else {
+         currentStart = currentStart+9;
+      }
+
+      for (let i = start; i < end; i++){
+         ul.appendChild(createStudentLI(i));
+         counter = counter++;
+      }
    }
 
 
+   //showPage();
 
    /*
    Create the `addPagination` function
    This function will create and insert/append the elements needed for the pagination buttons
    */
+   function addPagination(list){
+      const totalButtons = buttonTotal();
 
+      function createElement(elementName, property, value) {
+         const element = document.createElement(elementName);
+         element[property] = value;
+         return element;
+      } 
 
+      //Add the buttons 1 - X
+      for (let i = 1; i < (totalButtons+1); i++){
+         let li = createElement('li');
+         let button = createElement('button');
+         button.setAttribute("type", "button");
+         button.textContent = i;
 
+         if(i === 1){
+            button.setAttribute("className", "active");
+         }
+
+         li.appendChild(button);
+         ulButton.append(li);   
+      }   
+   }
 
    // Call functions
+   ulButton.addEventListener('click', (e) => {
+      if (e.target.tagName === 'BUTTON') {
+         const totalButtons = buttonTotal();
+         const button = e.target;
+         const li = button.parentNode;
+         const ul = li.parentNode;
+         
 
+         for (let i = 1; i < (totalButtons+1); i++){
+            button.removeAttribute("className");
+         }
+
+         //const button = e.target;
+         button.setAttribute("className", "active");
+      }
+      // showPage();
+      // addPagination(studentList);
+   });
+   
+   showPage();
+   addPagination(studentList);
 });
